c0680efd95606facb5c1f541c44ff0f7881651e2
thomasgrogan <thomasmgrogan1@gmail.com>
2023-04-11 16:18:27 +0100

    Task 2 complete

---
+++ src/components/App/Card/index.js
@@ -0,0 +1,7 @@
+// Inside index.js, create a functional component that returns a JSX element for your card.
+//Add a className className="card" to the JSX as there is a className allready applied to it in App.css (you can customize the CSS if you have time!).
+
+function NewCard() {
+    return <div className="card">This is the new card!</div>;
+}
+export default NewCard;
